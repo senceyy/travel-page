4f72afca2e856f267a265c0706e842942c79e7c7
senceyy <sencerduzenli@protonmail.com>
2021-06-25 07:42:15 -0700

    about section

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,7 @@
 const navMenu = document.querySelector('.Nav-menu')
 const navBtn = document.querySelector('.Nav-function-toggle')
 const closeBtn = document.querySelector('.Nav-menu-toggle')
+
 //Show menu
 if(navBtn) {
     navBtn.addEventListener('click', () => {
@@ -21,4 +22,12 @@ navMenuLink.forEach(element => {
 });
 function linkAction() {
     navMenu.classList.remove('show-menu')
+}
+//Change background header when scroll the page
+window.addEventListener('scroll', scrollHeader)
+function scrollHeader(){
+    const header = document.querySelector('.Header')
+    // When the scroll is greater than 100 viewport height, add the scroll-header class to the header tag
+    if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+    
 }
